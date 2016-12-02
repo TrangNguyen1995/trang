cc323193b8b0efdb5e8abd9496849615f4fcff32
ntdtrang <trangnguyen1121995@gmail.com>
2016-12-02 15:07:24 +0700

    Initial Project

---
+++ BTH9/BTH9/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace BTH9
+{
+    partial class frmCustomer
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvKhachHang = new System.Windows.Forms.DataGridView();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btCancel = new System.Windows.Forms.Button();
+            this.btSave = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvKhachHang)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dgvKhachHang
+            // 
+            this.dgvKhachHang.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvKhachHang.Dock = System.Windows.Forms.DockStyle.Top;
+            this.dgvKhachHang.Location = new System.Drawing.Point(0, 0);
+            this.dgvKhachHang.Name = "dgvKhachHang";
+            this.dgvKhachHang.Size = new System.Drawing.Size(585, 428);
+            this.dgvKhachHang.TabIndex = 0;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.btCancel);
+            this.panel1.Controls.Add(this.btSave);
+            this.panel1.Location = new System.Drawing.Point(0, 434);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(585, 65);
+            this.panel1.TabIndex = 1;
+            // 
+            // btCancel
+            // 
+            this.btCancel.Location = new System.Drawing.Point(320, 20);
+            this.btCancel.Name = "btCancel";
+            this.btCancel.Size = new System.Drawing.Size(75, 23);
+            this.btCancel.TabIndex = 1;
+            this.btCancel.Text = "Hủy";
+            this.btCancel.UseVisualStyleBackColor = true;
+            // 
+            // btSave
+            // 
+            this.btSave.Location = new System.Drawing.Point(188, 20);
+            this.btSave.Name = "btSave";
+            this.btSave.Size = new System.Drawing.Size(75, 23);
+            this.btSave.TabIndex = 0;
+            this.btSave.Text = "Lưu";
+            this.btSave.UseVisualStyleBackColor = true;
+            this.btSave.Click += new System.EventHandler(this.btSave_Click);
+            // 
+            // frmCustomer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(585, 499);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.dgvKhachHang);
+            this.Name = "frmCustomer";
+            this.Text = "Khach Hang";
+            this.Load += new System.EventHandler(this.frmCustomer_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvKhachHang)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvKhachHang;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btCancel;
+        private System.Windows.Forms.Button btSave;
+    }
+}
+
---
+++ BTH9/BTH9/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Configuration;
+namespace BTH9
+{
+    public partial class frmCustomer : Form
+    {
+        string cnStr = "";
+        SqlConnection cn;
+        DataSet ds;
+        public frmCustomer()
+        {
+            InitializeComponent();
+        }
+
+        private void frmCustomer_Load(object sender, EventArgs e)
+        {
+            cnStr = ConfigurationManager.ConnectionStrings["cnStr"].ConnectionString;
+            cn = new SqlConnection(cnStr);
+            dgvKhachHang.DataSource = GetCustomerDatase().Tables[0];
+        }
+
+        public DataSet GetCustomerDatase()
+        {
+            try
+            {
+                string sql = "SELECT * FROM KhachHang";
+                SqlDataAdapter da = new SqlDataAdapter(sql, cn);
+                /*DataSet*/ ds = new DataSet();
+                da.Fill(ds);
+                return ds;
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show(ex.Message);
+                return null;
+            }
+
+            finally
+            {
+                cn.Close(); 
+            }
+        }
+
+        private void btSave_Click(object sender, EventArgs e)
+        {
+            //////////INSERT
+            string ins = "INSERT INTO KhachHang(MaKH, TenKH, DiaChi, DienThoai, Fax) VALUES(@id, @name, @address, @phone, @fax)";
+            //string ins = "INSERT INTO KhachHang(MaKH) VALUES(@id)";
+
+            SqlDataAdapter da = new SqlDataAdapter(); //=>UPDATE
+            SqlCommand cmd = new SqlCommand(ins, cn);
+            cmd.Parameters.Add("@id",SqlDbType.NVarChar,4,"MaKH");
+            cmd.Parameters.Add("@name",SqlDbType.NVarChar,30,"TenKH");
+            cmd.Parameters.Add("@address",SqlDbType.NVarChar,30,"DiaChi");
+            cmd.Parameters.Add("@phone",SqlDbType.NVarChar,7,"DienThoai");
+            cmd.Parameters.Add("@fax",SqlDbType.NVarChar,12,"Fax");
+
+            da.InsertCommand = cmd;
+
+            //////////DELETE
+            string del = "DELETE FROM KhachHang WHERE MaKH = @id";
+            cmd = new SqlCommand(del, cn);
+            cmd.Parameters.Add("@id", SqlDbType.NVarChar, 4, "MaKH");
+            da.DeleteCommand = cmd;
+
+            //////////
+
+            da.Update(ds);
+
+            //Refresh
+            //dgvKhachHang.DataSource = GetCustomerDatase().Tables[0];
+        }
+    }
+}
